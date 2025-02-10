6fbd5915f9a346f614a4cdf22890a6b6063bf37b
Matthew Beckwith <mbeckwith@morganautogroup.com>
2025-02-10 16:08:35 -0500

    changed the message again... removed instructions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { createApp, ref, computed } = Vue;
 createApp({
     data(){
         return {
-            titleMsg: ref("...this is the secret message.  Hello Kiki."),
+            titleMsg: ref("Please Kiki, don't be mad at me... I didn't mean to ignore you... i didn't mean to leave you stranded down stairs all alone to carry your computer.... Please, Please, Please, forgive me!"),
             showMsg: ref(false),
 
             toggleMsg: () => {
