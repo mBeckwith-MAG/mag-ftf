3bcd14f2f0a0e23ad2547756cd67c162c40e04e5
Matthew Beckwith <kodahscripts@outlook.com>
2025-02-11 20:28:38 -0500

    made messages plural

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const { createApp, ref, computed } = Vue;
 createApp({
 	data() {
 		return {
-			titleMsg: ref(
-				'Hmm.. I wonder if you check this... Have fun Working from home :D'
-			),
+			messages: ref([
+				'Hmm.. I wonder if you check this... Have fun Working from home :D',
+				"I can just keep adding more... until you tell me you saw it... I'll just keep adding.",
+			]),
 			showMsg: ref(false),
 
 			toggleMsg: () => {
